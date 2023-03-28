a7327011e2b01595d9b9fa9f9479818b6395de82
MukilMuki <mukilan.shanmugasundaram@cdw.com>
2023-03-28 15:48:21 +0530

    Update index.js

--- Exercise-5/index.js
+++ Exercise-5/index.js
@@ -4,7 +4,7 @@ let cors=require('cors');
 
 app.use(cors(
     {
-        origin: ['http://localhost:4000','http://127.0.0.1:5500/sirius-locations/index.html'],
+        origin: ['http://127.0.0.1:5500/sirius-locations/index.html'],
     }
 ));
 app.listen(4000);
