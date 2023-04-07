7d3cf5935ba7c1144deddaf1a06ce1701ec572e8
MukilMuki <mukilan.shanmugasundaram@cdw.com>
2023-04-07 22:59:16 +0530

    changed after comments

--- Exercise-2/index.js
+++ Exercise-2/index.js
@@ -1,28 +1,57 @@
-let http = require("http");
-let fileSystem = require("fs");
-let colorPalette = require("./color-palette.json");
+let http = require("http"); //http package used to access the server related functions
+let fileSystem = require("fs-extra"); //filesystem package that is used to access file promises
+let colorPalette = require("./color-palette.json"); //Loaded the require json file into a variable
 
-function writeToFile() {
-  let colorsArray = JSON.parse(
-    fileSystem.readFileSync("./color-palette.json", "utf-8")
-  );
-  var randomColorsArray = [];
-  var unique = [];
-  while (unique.length < 5) {
-    var randomPalette = Math.floor(Math.random() * colorPalette.length);
-    randomColorsArray.push(colorsArray[randomPalette]);
-    unique = randomColorsArray.filter(
-      (value, index, array) => array.indexOf(value) === index
-    );
+//Async Function used to read a file from a specific path and returns the said file back
+async function readFromFile(path) {
+  try {
+    const file = await fileSystem.readFile(path, "utf-8");
+    return file;
+  } catch (err) {
+    console.log(err);
   }
-  fileSystem.writeFileSync("./random-colors.json", JSON.stringify(unique));
 }
 
+//Async Function used to write the random generated colors into a file
+async function writeToFile(JSONData) {
+  try {
+    await fileSystem.writeFile(
+      "./random-colors.json",
+      JSON.stringify(JSONData)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//Async Function used to generate random colors that are unique
+async function generateRandomColors() {
+  try {
+    let colorsArray = JSON.parse(await readFromFile("./color-palette.json"));
+    var randomColorsArray = [];
+    while (randomColorsArray.length < 5) {
+      var randomPalette = Math.floor(Math.random() * colorPalette.length);
+      randomColorsArray.push(colorsArray[randomPalette]);
+      randomColorsArray = randomColorsArray.filter(
+        (value, index, array) => array.indexOf(value) === index
+      );
+    }
+    writeToFile(randomColorsArray);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//Creation of server and writing the colors generated from the file into the server response
 http
-  .createServer((req, res, err) => {
-    writeToFile();
-    const data = fileSystem.readFileSync("./random-colors.json", "utf-8");
-    res.write(data);
-    res.end();
+  .createServer(async (req, res, err) => {
+    try {
+      generateRandomColors();
+      const fileData = await readFromFile("./random-colors.json");
+      res.write(fileData);
+      res.end();
+    } catch (err) {
+      res.write(err);
+    }
   })
   .listen(4000);
